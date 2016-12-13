8f74f6ab4a9c2e42154b768fcff9e26238947596
EduStrife <47883089x@NOVAULAS.STUCOM.COM>
2016-12-13 21:14:06 +0100

    Ex05 array

--- Arrays/src/Ex05.java
+++ Arrays/src/Ex05.java
@@ -1,4 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Ex05 {
 
+public static void main(String[] args) {
+		
+		try{
+			
+			int num;
+			int[] numeros = new int[10]; // array numeros
+			 
+			BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("Introduce 10 números enteros");
+			
+			for(int i=0; i<numeros.length; i++){
+				num = Integer.parseInt(buffer.readLine());
+				numeros[i]=num;
+			}
+			
+			for(int i : numeros) System.out.println(i);
+		}
+		
+		catch (Exception e){
+			System.out.println("Error al introducir los números " + e);
+		}
+
+	}
+	
 }

b4c23a4b2b487913fb6062a457912784833c7d08
EduStrife <edustrife@gmail.com>
2017-01-25 23:57:14 +0100

    hormigas

---
+++ Arrays/src/hormigas.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class hormigas {
+	public static void main(String[] args) {
+
+		try{
+			BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+			
+	
+			
+
+		}
+
+		catch (Exception e){
+			System.out.println("Error al introducir los datos" + e);
+		}
+	}
+}
